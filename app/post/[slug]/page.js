17947a5f64e35bafe2a1047c59f5e20e85b1edab
aansariaxioned <ansaria@axioned.com>
2025-05-02 20:15:04 +0530

    completed

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -11,8 +11,7 @@ export default function PostPage() {
 
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
-
-
+  
   async function fetchPost(id) {
     try {
       const res = await fetch(`https://dummyjson.com/recipes/${id}`);
@@ -28,30 +27,22 @@ export default function PostPage() {
   }
 
   useEffect(() => {
-
     fetchPost(id.slug);
   }, [id]);
 
-
-
   return (
-
     <div className="p-10 max-w-3xl mx-auto">
-
       {loading ? (
         <div className="h-96 flex justify-center items-center">
-
           <div className="w-20 h-20 border-4 border-blue-500 border-t-transparent border-b-transparent rounded-full animate-spin"></div>
         </div>
 
       ) : (
         <div>
+          <div>
+            <Image src={post.image} width={600} height={400} alt={post.name} className="rounded-xl mb-6" />
+          </div>
 
-       
-            <div>
-              <Image src={post.image} width={600} height={400} alt={post.name} className="rounded-xl mb-6" />
-            </div>
-          
 
 
           <h1 className="text-4xl font-bold mb-4">{post.name}</h1>
